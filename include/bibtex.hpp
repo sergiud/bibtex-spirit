00ef54adda3a3318433ed329e5fbfc6f9e8859bd
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2011-04-12 16:58:29 +0200

    allow multiple values in a single entry

--- include/bibtex.hpp
+++ include/bibtex.hpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2011 Sergiu Dotenco
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 /**
  * @brief Boost.Spirit v2 based BibTeX parser.
  * @file bibtex.hpp
@@ -108,10 +128,28 @@ public:
 
         value_
             = quotedValue_
-            | +(~qi::char_(",})"))
+            | +~qi::char_(",})#")
             ;
 
-        entry_ = key_ >> '=' >> value_;
+        entry_
+            = key_
+            [
+                ph::at_c<0>(_val) = _1
+            ]
+            >> '=' >> (value_ % '#')
+            [
+                ph::at_c<1>(_val) =
+                    ph::accumulate(_1, ph::construct<KeyValue::second_type>(),
+                        ph::lambda[
+                            ph::if_else(ph::empty(ph::arg_names::_1),
+                                ph::arg_names::_2,
+                                ph::arg_names::_1 + ph::val(" # ") +
+                                    ph::arg_names::_2)
+                        ]
+                    )
+            ]
+            ;
+
         entries_ = -(entry_ % ',');
 
         body_
--- tests/parse/main.cpp
+++ tests/parse/main.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2011 Sergiu Dotenco
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #define BOOST_TEST_MODULE BibTeX
 
 #include <vector>
@@ -275,4 +295,28 @@ BOOST_AUTO_TEST_CASE(mixed_multiple)
     BOOST_REQUIRE_EQUAL(e.size(), 2);
 
     BOOST_CHECK(e[0] == e[1]);
+}
+
+BOOST_AUTO_TEST_CASE(separated)
+{
+    BibTeXEntry e;
+
+    const char test[] =
+        "@book{abc1,"
+        "   a = {b asd asd\nsecond line\nthird line adas das },\n"
+        "   c = %{d asd adasd a}, d =\n {d asd \\} adasd a},"
+        "   d123 = \"test test\\\" aa\","
+        "   bcd-e = ack,"
+        "   more = abc # dfg # { , } # a,"
+        "   normal = test,"
+        "   last = \"last\" # { one}"
+        "}";
+
+    BOOST_REQUIRE(parse(boost::make_iterator_range(test), e));
+    BOOST_REQUIRE_EQUAL(e.entries.size(), 7);
+
+    BOOST_CHECK_EQUAL(e.entries[4].first, "more");
+    BOOST_CHECK_EQUAL(e.entries[4].second, "abc # dfg #  ,  # a");
+    BOOST_CHECK_EQUAL(e.entries[6].first, "last");
+    BOOST_CHECK_EQUAL(e.entries[6].second, "last #  one");
 }
